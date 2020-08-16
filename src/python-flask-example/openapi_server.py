49ab981922b8eff6bc9c26ea03d61fc23aa50261
Tom Wilson <tom.andrew.wilson@gmail.com>
2020-08-16 15:28:16 -0400

    update path to openapi.yaml

--- src/python-flask-example/openapi_server.py
+++ src/python-flask-example/openapi_server.py
@@ -9,7 +9,7 @@ from openapi_server import encoder
 
 
 def main():
-    app = connexion.App(__name__, specification_dir='./openapi/')
+    app = connexion.App(__name__, specification_dir='./openapi_server/openapi/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('openapi.yaml',
                 arguments={'title': 'Swagger Petstore'},
